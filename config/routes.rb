1c6109c72358219f434b44ef834f4bfe9c127764
Baiyu Seawave <chenbaiyu2065@gmail.com>
2011-05-08 14:44:05 +0800

    Finished first cut of the User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 SampleApp::Application.routes.draw do
+  resources :users
 
-  get "users/new"
+  ###  get "users/new"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
